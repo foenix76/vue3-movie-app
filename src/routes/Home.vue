ed64a3162e48dc324a5e434d44fc308a32f56a12
5800X <foenix76@gmail.com>
2025-01-07 19:27:54 +0900

    검색 인풋 및 셀렉트박스 3개 작업 타입, 페이지당카운트, 연도

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <Headline />
+  <Search />
 </template>
 
 <script>
 import Headline from '~/components/Headline'
+import Search from '~/components/Search'
 
 export default {
   // component로 s하나 빼먹어서 30분 넘게 잡아먹음 T^T
   components: {
-    Headline
+    Headline,
+    Search
   }
 }
 </script>
